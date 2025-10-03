0ea46adc4ee6797adf206de5d9292112fe0029f9
dollentine <rubyaa2@nycstudents.net>
2025-10-03 00:00:57 +0000

    Updated TipCalculator logic

--- src/main/java/com/example/TipCalculator.java
+++ src/main/java/com/example/TipCalculator.java
@@ -3,28 +3,40 @@ import java.util.Scanner;
 
 public class TipCalculator {
     //WRITE YOUR PROGRAM IN calculateTip
-    public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
+         public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
+         
+         double tip = Math.round (cost * ((percent / 100.0)) * 100) / 100.0; 
+         double billWithTip = Math.round ((tip + cost) * 100) / 100.0; 
+         double perPersonBeforeTip = Math.round ((cost / people) * 100) / 100.0; 
+         double tipPerPerson = Math.round ((tip / people) * 100) / 100.0; 
+         double totalCostPerPerson = Math.round ((billWithTip / people) * 100)/ 100.0; 
+
         //DO NOT DELETE ANY OF THE CODE BELOW      
 
         String result = "-------------------------------\n" +
-                       "Total bill before tip: $" + "" + "\n" +
-                       "Total percentage: " + "" + "%\n" +
-                       "Total tip: $" + "" + "\n" +
-                       "Total Bill with tip: $" + "" + "\n" +
-                       "Per person cost before tip: $" + "" + "\n" +
-                       "Tip per person: $" + "" + "\n" +
-                       "Total cost per person: $" + "" + "\n" +
+                       "Total bill before tip: $" + cost + "" + "\n" +
+                       "Total percentage: "  + percent + "" + "%\n" +
+                       "Total tip: $" + tip + "" + "\n" +
+                       "Total Bill with tip: $" + billWithTip + "" + "\n" +
+                       "Per person cost before tip: $" + perPersonBeforeTip + "" + "\n" +
+                       "Tip per person: $" + tipPerPerson + "" + "\n" +
+                       "Total cost per person: $" + totalCostPerPerson + "" + "\n" +
                        "-------------------------------\n";
 
         return result;
-    }
+            
+         }
+
+
+        //DO NOT DELETE ANY OF THE CODE BELOW      
+
 
     public static String extraCredit(int people, int percent, double cost) {
         String result = "Extra credit not implemented";
         boolean condition = true;
     
         //  COPY AND PASTE YOUR PROGRAM FROM calculateTip() HERE 
-        
+        System.out.println  ("Test"); 
         // the while loop condition is checked,
         // and if TRUE, runs the code inside.
         // when the code inside is done running, the condition is rechecked,
@@ -50,6 +62,7 @@ public class TipCalculator {
         double cost=10.5;              
         //System.out.println(calculateTip(people,percent,cost));
         System.out.println(extraCredit(people, percent, cost));
+        System.out.println(calculateTip(6, 25, 52.27));
     }
 }
         
